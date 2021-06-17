cd4deba0f5df9eacc58ac2c09554923685b8ea63
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2021-06-17 14:12:54 +0200

    Hashvatar

---
+++ components/Hashvatar/index.ts
@@ -0,0 +1,1 @@
+export { SHA256Avatar as default } from "./Hashvatar"; 
---
+++ components/Svg/index.ts
@@ -0,0 +1,2 @@
+export { Svg as default } from "./Svg";
+//Stolen from https://github.com/47ng/chakra-next/blob/v3/src/components/svg.tsx
