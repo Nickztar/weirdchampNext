ae35e4b60c2c48b366bc6553174bb1ae748e52e2
Nicholas Broström <nicholas.brostrom@effectsoft.se>
2021-06-21 16:09:06 +0200

    discord icon on login

--- components/LandingPage/LandingPage.tsx
+++ components/LandingPage/LandingPage.tsx
@@ -7,6 +7,7 @@ import {
   Icon,
   Flex,
 } from '@chakra-ui/react';
+import {FaDiscord} from "react-icons/fa";
 
 export const LandingPage: React.FC = (): React.ReactElement => (
   <Flex
@@ -61,13 +62,7 @@ export const LandingPage: React.FC = (): React.ReactElement => (
         size="lg"
       >
         Log in with discord
-        <Icon boxSize={4} ml={1} viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </Icon>
+        <FaDiscord size={20} style={{color: "inherit", marginLeft: "var(--chakra-space-2)"}} />
       </Button>
     </Box>
   </Flex>
